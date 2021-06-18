377afdfc928dc6b01c6fd815caca143679cfa149
[jiansorge] <[ji@nsorge.com]>
2021-06-18 00:12:07 -0700

    renders images from api call

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-let imgs = []
-
 fetch('http://localhost:8080/data/photos.json', {
   mode: 'cors',
 })
-.then(res=>res.json())
-.then(res=>console.log(res))
-//.then((res)=>imgs=[...res])
+.then(res => res.json())
+//.then(res => console.log("res:",res))
+.then(data => {
+  const container = document.getElementById('image-container');
+  const docFrag = document.createDocumentFragment();
+
+  data.forEach(function(imageObject, index, originalArray) {
+      const img = document.createElement('img');
+      img.src = imageObject.urls.small;
+      docFrag.appendChild(img);
+      console.log("img",index,img)
+  });
+
+  container.appendChild(docFrag);
+})
+.then(console.log("imageObjects:",imgObjects))
 .catch(e=>console.log(e));
 
-const container = document.getElementById('image-container');
-const docFrag = document.createDocumentFragment();
-
-imgs.forEach(function(imageObject, index, originalArray) {
-    const img = document.createElement('img');
-    img.src = imageObject.urls.regular;
-    docFrag.appendChild(img);
-});
-
-container.appendChild(docFrag);
